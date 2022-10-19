8a9b6e729984b9faf3778c81ead3218304497427
ali <alirezanoori02121@gmail.com>
2022-10-19 14:35:46 +0100

    using scanner

--- src/main/java/com/lbg/coh2/App.java
+++ src/main/java/com/lbg/coh2/App.java
@@ -1,13 +1,24 @@
 package com.lbg.coh2;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
+    	
         System.out.println( "Hello World!" );
+        int x= System.in.read();
+        System.out.println("=>" + x );
+        Scanner console = new Scanner(System.in);
+        String y=console.next();
+        System.out.println("=>" + y );
+        
     }
+   
 }
