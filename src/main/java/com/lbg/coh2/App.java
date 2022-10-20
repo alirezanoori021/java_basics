ef06870adde2d23974957e93c7e3dd345c1e8177
ali <alirezanoori02121@gmail.com>
2022-10-20 14:46:05 +0100

    Add arrayList

--- src/main/java/com/lbg/coh2/App.java
+++ src/main/java/com/lbg/coh2/App.java
@@ -1,6 +1,10 @@
 package com.lbg.coh2;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 /**
@@ -11,14 +15,43 @@ public class App
 {
     public static void main( String[] args ) throws IOException
     {
-    	
-        System.out.println( "Hello World!" );
-        int x= System.in.read();
-        System.out.println("=>" + x );
-        Scanner console = new Scanner(System.in);
-        String y=console.next();
-        System.out.println("=>" + y );
+    	 App app= new App();
+    	 String lineRead="x";
+    	 Scanner console = new Scanner(System.in);
+    	 ArrayList<Double> lines = new ArrayList<Double>();    	
+    	 System.out.println( "Enter the VAT percentage  :" );
+         float vat =console.nextFloat();
+        try
+        {
+            while(prompt() &&(lineRead = console.next()) != null) 
+            {
+                if (lineRead.toString().equalsIgnoreCase("QUIT"))
+                	break;
+
+            	lines.add(app.VAT(Float.parseFloat(lineRead),vat));
+            }
+            
+        }catch( NoSuchElementException e )
+        {}
+        
+        Collections.sort(lines);
+        Collections.reverse(lines);
+        for (double line : lines) {
+        System.out.println(line);
+        }
+        
         
     }
-   
+
+    
+   double VAT (float cost, float vat) {
+
+//     //return ((((vat)/100)*cost) + cost);
+    return ((vat*cost)/100 + cost)	;	
+    }
+   static private boolean prompt()
+   {
+       System.out.print("enter a message :");
+       return true;
+   }
 }
